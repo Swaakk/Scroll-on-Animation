c67658f2e4f08a453a7b5bbf0fd119851b12e83c
Swaakk <swathykrishna.thayyil@gmail.com>
2022-03-21 12:32:12 +0530

    shed designing

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -3,16 +3,43 @@ import "../styles/MainContainer.css"
 import AOS from "aos"
 import "aos/dist/aos.css";
 
+
+
 function MainContainer() {
+  useEffect(()=>{
+    AOS.init({
+      duration : 1000,
+      easing : "ease",
+      once : false
+    });
+  })
   return (
     <div className='MainContainer'>
-      <div className='box'></div>
-      <div className='box'></div>
-      <div className='box'></div>
-      <div className='box'></div>
-      <div className='box'></div>
-      <div className='box'></div>
-      <div className='box'></div>
+      <div className='box' data-aos="fade-up"></div>
+      <div className='box' data-aos="slide-right"></div>
+      <div className='box' data-aos="slide-left"></div>
+      <div className='box' data-aos="zoom-out-right"></div>
+      <div className='box' data-aos="flip-left"></div>
+      <div className='box' data-aos="fade-down-left"></div>
+      <div className='box' data-aos="slide-left"></div>
+      <div className='box' data-aos="flip-left"></div>
+      <div className='box' data-aos="fade-down-left"></div>
+      <div className='box' data-aos="slide-left"></div>
+      <div className='box' data-aos="flip-left"></div>
+      <div className='box' data-aos="fade-down-left"></div>
+      <div className='box' data-aos="slide-left"></div>
+      <div className='box' data-aos="flip-left"></div>
+      <div className='box' data-aos="fade-down-left"></div>
+      <div className='box' data-aos="slide-left"></div>
+      <div className='box' data-aos="flip-left"></div>
+      <div className='box' data-aos="fade-down-left"></div>
+      <div className='box' data-aos="slide-left"></div>
+      <div className='box' data-aos="flip-left"></div>
+      <div className='box' data-aos="fade-down-left"></div>
+      <div className='box' data-aos="slide-left"></div>
+      <div className='box' data-aos="flip-left"></div>
+      <div className='box' data-aos="fade-down-left"></div>
+      <div className='box' data-aos="slide-left"></div>
       
          
       
